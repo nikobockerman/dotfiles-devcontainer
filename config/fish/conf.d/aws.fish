3fed1f922c332e01f71030a987aff56d7170f0f3
Niko Böckerman <git.585wj@slmail.me>
2024-12-06 14:21:31 +0000

    Silence when aws_completer is missing
    aws_completer is only available in relevant repositories. It's
    preferable to not see this message in all others.

--- config/fish/conf.d/aws.fish
+++ config/fish/conf.d/aws.fish
@@ -1,6 +1,7 @@
 set -e aws_profile
 
-if not set -q aws_completer_path
-  set -g aws_completer_path (type -P aws_completer 2> /dev/null)
-    or echo "aws: unable to find aws_completer, completions unavailable"
+if type -q aws_completer
+  if not set -q aws_completer_path
+    set -g aws_completer_path (type -P aws_completer)
+  end
 end
